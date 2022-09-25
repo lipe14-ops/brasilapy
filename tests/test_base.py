063d14f39a7edf9af7f8d7afbbe28ff23b024599
Jonhnatha Trigueiro <joepreludian@gmail.com>
2022-09-25 02:01:57 -0300

    Update test_base

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,4 +1,3 @@
-import pytest
 from unittest import mock
 
 from brasilapy import BrasilAPI
@@ -6,14 +5,17 @@ from brasilapy.models import Bank
 
 
 class TestBaseRequests:
-
     def test_get_banks(self, api_client, request_banks_dict):
         brasil_api: BrasilAPI = api_client
 
-        with mock.patch("brasilapy.client.RequestsProcessor.get_data", return_value=request_banks_dict) as get_data_mock:
+        with mock.patch(
+            "brasilapy.client.RequestsProcessor.get_data",
+            return_value=request_banks_dict,
+        ) as get_data_mock:
 
             banks = brasil_api.get_banks()
 
+            get_data_mock.assert_called_once()
             assert len(banks) == len(request_banks_dict)
             assert banks[0].dict() == request_banks_dict[0]
             assert type(banks[0]) is Bank
