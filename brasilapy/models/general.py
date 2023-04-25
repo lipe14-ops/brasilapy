e1e938dfa2d887c5ac5c49b952ac0997abf47919
Jêfferson Maxwell <jeffersonmaxwell1603@gmail.com>
2023-04-25 05:36:35 +0000

    add function get_corretora() and get_corretoras().

--- brasilapy/models/general.py
+++ brasilapy/models/general.py
@@ -107,3 +107,25 @@ class RegistroBrDominio(BaseReturnModel):
 class TaxaJuros(BaseReturnModel):
     nome: str
     valor: float
+
+
+class Corretoras(BaseReturnModel):
+    bairro: str
+    cep: str
+    cnpj: str
+    codigo_cvm: str
+    complemento: str
+    data_inicio_situacao: str
+    data_patrimonio_liquido: str
+    data_registro: str
+    email: str
+    logradouro: str
+    municipio: str
+    nome_social: str
+    nome_comercial: str
+    pais: str
+    status: str
+    telefone: str
+    type: str
+    uf: str
+    valor_patrimonio_liquido: str
---
+++ tests/corretoras/conftest.py
@@ -0,0 +1,29 @@
+import pytest
+
+
+@pytest.fixture
+def request_example_list():
+    return [
+            {
+                    "bairro": "LEBLON",
+                    "cep": "22440032",
+                    "cnpj": "02332886000104",
+                    "codigo_cvm": "3247",
+                    "complemento": "SALA 201",
+                    "data_inicio_situacao": "1998-02-10",
+                    "data_patrimonio_liquido": "2021-12-31",
+                    "data_registro": "1997-12-05",
+                    "email": "juridico.regulatorio@xpi.com.br",
+                    "logradouro": "AVENIDA ATAULFO DE PAIVA 153",
+                    "municipio": "RIO DE JANEIRO",
+                    "nome_social": "XP INVESTIMENTOS CCTVM S.A.",
+                    "nome_comercial": "XP INVESTIMENTOS",
+                    "pais": "",
+                    "status": "EM FUNCIONAMENTO NORMAL",
+                    "telefone": "30272237",
+                    "type": "CORRETORAS",
+                    "uf": "RJ",
+                    "valor_patrimonio_liquido": "5514593491.29"
+            }
+    ]
+
